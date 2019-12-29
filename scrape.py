3d6dd3942cc7d529ab095b605dd6c1846d091881
jxu <7989982+jxu@users.noreply.github.com>
2019-12-29 02:47:06 -0500

    Scrape both old and new sites instead of one or the other

--- scrape.py
+++ scrape.py
@@ -12,36 +12,42 @@ OLDSITE_END_DATE = date(2018, 3, 23)  # last snapshot before redirect
 NEWSITE_START_DATE = date(2017, 6, 17)
 
 
-query_date = OLDSITE_START_DATE
-snapshot_dates = set()
-os.makedirs("snapshots", exist_ok=True)
+def query_ia(start_date, end_date, base_site):
+    """Iterate through [start_date, end_date] and saves snapshots"""
+    query_date = start_date
+    snapshot_dates = set()
 
-while query_date < date.today():
-    query_date_str = query_date.strftime("%Y%m%d")
+    while query_date <= end_date:
+        query_date_str = query_date.strftime("%Y%m%d")
 
-    # Query old site or new site based on query date
-    if query_date < NEWSITE_START_DATE:
-        base_site = "https://osu.ppy.sh/p/pp"
-    else:
-        base_site = "osu.ppy.sh/rankings/osu/performance"
+        # Use feature where putting in the date str redirects to closest
+        # date snapshot
+        # Can make requests async, but don't to not generate too much activity
+        query_url = f"https://web.archive.org/web/{query_date_str}/{base_site}"
+        r = requests.get(query_url)
+        snapshot_timestamp = ''.join(c for c in r.url if c.isdigit())
+        snapshot_date = snapshot_timestamp[:8]
 
-    # Use feature where putting in the date str redirects to closest
-    # date snapshot
-    # Can make requests async, but don't to not generate too much activity
-    query_url = f"https://web.archive.org/web/{query_date_str}/{base_site}"
-    r = requests.get(query_url)
-    snapshot_timestamp = ''.join(c for c in r.url if c.isdigit())
-    snapshot_date = snapshot_timestamp[:8]
+        print(f"Queried {query_date}, got snapshot date {snapshot_date}")
 
-    print(f"Queried {query_date}, got snapshot date {snapshot_date}")
+        if snapshot_date not in snapshot_dates:
+            snapshot_dates.add(snapshot_date)
+            snapshot_path = f"snapshots/{snapshot_date}.html"
+            print("Writing", snapshot_path)
+            with open(snapshot_path, 'w') as f:
+                f.write(r.text)
 
-    if snapshot_date not in snapshot_dates:
-        snapshot_dates.add(snapshot_date)
-        snapshot_path = f"snapshots/{snapshot_date}.html"
-        print("Writing", snapshot_path)
-        with open(snapshot_path, 'w') as f:
-            f.write(r.text)
-
-    query_date += timedelta(days=1)
+        query_date += timedelta(days=1)
 
 
+def main():
+    # hardcoded directory oh well
+    os.makedirs("snapshots", exist_ok=True)
+
+    query_ia(OLDSITE_START_DATE, OLDSITE_END_DATE, "osu.ppy.sh/p/pp")
+    # Will overwrite a few overlapping snapshots
+    query_ia(NEWSITE_START_DATE, date.today(),
+             "osu.ppy.sh/rankings/osu/performance")
+
+if __name__ == '__main__':
+    main()
