70e38431ea0a57d10aba78896b399532250472bf
jxu <7989982+jxu@users.noreply.github.com>
2019-12-30 16:35:54 -0500

    Add in plotly top 10 graph and fix missing usernames

--- visualize.R
+++ visualize.R
@@ -6,7 +6,7 @@ player_history <- read_csv("player_history.csv")
 # convert date to date object
 player_history <- 
   player_history %>% 
-  mutate(date = ymd(date))
+  mutate(date = ymd(date)) 
 
 # user NUMBA WAN blatant cheater
 # popular username mostly matches current username
@@ -19,13 +19,22 @@ username_map <-
 
 player_history <- 
   player_history %>%
-  full_join(username_map, by = "id")
+  left_join(username_map, by = "id")
 
 # to not connect non-consecutive dates, create new rows: every combination of player and date
+# and add in usernames again
 player_history_10 <- 
   player_history %>%
   filter(rank <= 10) %>%
-  complete(id, date)
+  complete(id, date) %>%
+  select(-popular_username, -current_username) %>%
+  left_join(username_map, by = "id")
 
+
+library(plotly)
 ggplot(player_history_10, aes(x = date, y = pp, group = id, color = popular_username)) +
-  geom_line()
+  geom_line() + 
+  geom_vline(xintercept = ymd("20140127"), color = "gray") +
+  annotate("text", x = ymd("20140127")+50, y = 6000, label = "ppv2")
+
+ggplotly()
