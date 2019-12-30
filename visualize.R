125d88d12476a668c8e9e67ade3f0bdecffb149c
jxu <7989982+jxu@users.noreply.github.com>
2019-12-30 03:37:14 -0500

    Fix top 10 graph to not connect non-consecutive data points

--- visualize.R
+++ visualize.R
@@ -3,7 +3,7 @@ library(lubridate)
 
 player_history <- read_csv("player_history.csv")
 
-# convert to date object
+# convert date to date object
 player_history <- 
   player_history %>% 
   mutate(date = ymd(date))
@@ -21,10 +21,11 @@ player_history <-
   player_history %>%
   full_join(username_map, by = "id")
 
-
+# to not connect non-consecutive dates, create new rows: every combination of player and date
 player_history_10 <- 
   player_history %>%
-  filter(rank <= 10)
+  filter(rank <= 10) %>%
+  complete(id, date)
 
 ggplot(player_history_10, aes(x = date, y = pp, group = id, color = popular_username)) +
   geom_line()
