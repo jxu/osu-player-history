da2b370937c249f3bb73b056997ab4611e73a2ea
jxu <7989982+jxu@users.noreply.github.com>
2020-01-01 02:28:11 -0500

    Add bar plot race experiments

--- visualize.R
+++ visualize.R
@@ -1,5 +1,7 @@
+library(plotly)
 library(tidyverse)
 library(lubridate)
+library(gganimate)
 
 player_history <- read_csv("player_history.csv")
 
@@ -21,20 +23,47 @@ player_history <-
   player_history %>%
   left_join(username_map, by = "id")
 
-# to not connect non-consecutive dates, create new rows: every combination of player and date
-# and add in usernames again
+
 player_history_10 <- 
   player_history %>%
-  filter(rank <= 10) %>%
+  filter(rank <= 10) 
+
+# to not connect non-consecutive dates, create new rows: every combination of player and date
+# and add in usernames again
+player_history_10_complete <- 
+  player_history_10 %>%
   complete(id, date) %>%
   select(-popular_username, -current_username) %>%
   left_join(username_map, by = "id")
 
-
-library(plotly)
-ggplot(player_history_10, aes(x = date, y = pp, group = id, color = popular_username)) +
+# top 10 line graphs
+ggplot(player_history_10_complete, 
+       aes(x = date, y = pp, group = id, color = popular_username)) +
   geom_line() + 
   geom_vline(xintercept = ymd("20140127"), color = "gray") +
   annotate("text", x = ymd("20140127")+50, y = 6000, label = "ppv2")
 
 ggplotly()
+
+# testing data
+df <- player_history_10 %>% 
+  filter(date <= ymd("20120501")) 
+
+# animated bar chart (coord_flip is bugged)
+p <- ggplot(df, aes(y = rank, group = id)) + 
+  geom_tile(aes(x = pp / 2,
+                height = 0.9, width = pp), fill = "pink") +
+  geom_text(aes(x = 6000, label = username), 
+            hjust = 0) + 
+  geom_text(aes(x = pp, label = as.character(pp)), # tweening creates decimals (issue #204)
+            hjust = 1) +
+  ggtitle("{closest_state}") + 
+  scale_y_reverse() +
+  #coord_cartesian(expand = FALSE) + 
+  view_step_manual(xmin = 6000, xmax = 7000, ymin = 1, ymax = 10) +
+  theme_minimal() +
+  transition_states(date, transition_length = 1, state_length = 1, wrap = FALSE) + 
+  ease_aes("cubic-in-out")
+
+animate(p, fps = 10, duration = 1)
+ 
