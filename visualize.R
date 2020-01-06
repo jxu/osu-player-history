de0f3a28c5fee075bf518825c5947390d55784be
jxu <7989982+jxu@users.noreply.github.com>
2020-01-06 16:22:08 -0500

    "Test interpolation in visualize"

--- visualize.R
+++ visualize.R
@@ -2,6 +2,7 @@ library(plotly)
 library(tidyverse)
 library(lubridate)
 library(gganimate)
+library(tweenr)
 
 player_history <- read_csv("player_history.csv")
 
@@ -10,7 +11,6 @@ player_history <-
   player_history %>% 
   mutate(date = ymd(date)) 
 
-# user NUMBA WAN blatant cheater
 # popular username mostly matches current username
 username_map <- 
   player_history %>%
@@ -19,35 +19,41 @@ username_map <-
               username %>% table %>% which.max %>% names,
             current_username = username[which.max(date)])
 
-player_history <- 
-  player_history %>%
-  left_join(username_map, by = "id")
 
+#player_history <- 
+#  player_history %>%
+#  left_join(username_map, by = "id")
 
-player_history_10 <- 
-  player_history %>%
-  filter(rank <= 10) 
-
-# to not connect non-consecutive dates, create new rows: every combination of player and date
+# To make plotting easier, create new rows: every combination of player and date
 # and add in usernames again
-player_history_10_complete <- 
-  player_history_10 %>%
-  complete(id, date) %>%
-  select(-popular_username, -current_username) %>%
-  left_join(username_map, by = "id")
+bar_race <- 
+  player_history %>%
+  filter(rank <= 10) %>%
+  complete(id, date)
 
-# top 10 line graphs
-ggplot(player_history_10_complete, 
-       aes(x = date, y = pp, group = id, color = popular_username)) +
-  geom_line() + 
-  geom_vline(xintercept = ymd("20140127"), color = "gray") +
-  annotate("text", x = ymd("20140127")+50, y = 6000, label = "ppv2")
+# Interpolate values (pp, etc.) only if consecutive snapshots are in top 10
 
-ggplotly()
+test_df <- 
+  bar_race %>%
+  filter(id == 213014) %>%
+  arrange(date) %>%
+  mutate(in_top = !is.na(rank),
+         mask = lead(in_top, default = FALSE) & in_top) %>%
+  complete(date = seq(min(date), max(date), by = "day")) %>%
+  fill(mask, id) %>%
+  mutate(mask = (mask | in_top) %>% replace_na(FALSE),
+         pp = if_else(mask, pp %>% tween_fill("linear"), NA_real_),
+         accuracy = if_else(mask, accuracy %>% tween_fill("linear"), NA_real_),
+         playcount = if_else(mask, playcount %>% tween_fill("linear"), NA_real_))
+
+
 
 # testing data
+# handling rank >= 10 to fly in from bottom
+# need to handle imputing username
 df <- player_history_10 %>% 
-  filter(date <= ymd("20120501")) 
+  filter(date <= ymd("20130101")) %>%
+  complete(id, date, fill = list(rank = 1000, pp = 0)) 
 
 # animated bar chart (coord_flip is bugged)
 p <- ggplot(df, aes(y = rank, group = id)) + 
@@ -65,5 +71,5 @@ p <- ggplot(df, aes(y = rank, group = id)) +
   transition_states(date, transition_length = 1, state_length = 1, wrap = FALSE) + 
   ease_aes("cubic-in-out")
 
-animate(p, fps = 10, duration = 1)
+animate(p, fps = 10, duration = 40)
  
