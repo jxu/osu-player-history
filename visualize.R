2eaad96942d19f3de0e537e6f5c057033e9936a0
jxu <7989982+jxu@users.noreply.github.com>
2020-07-18 05:22:47 -0400

    Experiment more with manual transition and tweening

--- visualize.R
+++ visualize.R
@@ -10,7 +10,7 @@ player_history <-
   player_history %>% 
   mutate(date = ymd(date)) 
 
-# popular username mostly matches current username
+# maybe useful: popular username mostly matches current username
 username_map <- 
   player_history %>%
   group_by(id) %>%
@@ -19,38 +19,28 @@ username_map <-
             current_username = username[which.max(date)])
 
 
-#player_history <- 
-#  player_history %>%
-#  left_join(username_map, by = "id")
-
 
 # Interpolate values (pp, etc.) *only if* consecutive snapshots are in top 10
+# probably not going to use as it's too complicated
 # Mostly constant variables (username, country) use LOCF
-interpolate_consecutive <- function(df) {
-  df %>%
-    arrange(date) %>%
-    mutate(in_snapshot = !is.na(rank),
-           in_top = lead(in_snapshot, default = FALSE) & in_snapshot) %>%
-    complete(date = seq(min(date), max(date), by = "day"),
-             fill = list(in_snapshot = FALSE)) %>%
-    fill(in_top, 
-         id,
-         country,
-         username) %>%
-    # possibly use mutate_at
-    mutate(in_top = (in_top | in_snapshot),
-           pp = if_else(in_top, pp %>% tween_fill("linear"), NA_real_),
-           accuracy = if_else(in_top, accuracy %>% tween_fill("linear"), NA_real_),
-           playcount = if_else(in_top, playcount %>% tween_fill("linear"), NA_real_)) 
-}
+# interpolate_consecutive <- function(df) {
+#   df %>%
+#     arrange(date) %>%
+#     mutate(in_snapshot = !is.na(rank),
+#            in_top = lead(in_snapshot, default = FALSE) & in_snapshot) %>%
+#     complete(date = seq(min(date), max(date), by = "day"),
+#              fill = list(in_snapshot = FALSE)) %>%
+#     fill(in_top, 
+#          id,
+#          country,
+#          username) %>%
+#     # possibly use mutate_at
+#     mutate(in_top = (in_top | in_snapshot),
+#            pp = if_else(in_top, pp %>% tween_fill("linear"), NA_real_),
+#            accuracy = if_else(in_top, accuracy %>% tween_fill("linear"), NA_real_),
+#            playcount = if_else(in_top, playcount %>% tween_fill("linear"), NA_real_)) 
+# }
 
-# test
-top_n <- 10
-df <- bar_race %>% filter(date == ymd("20120422")) 
-# arrange puts NAs at bottom
-
-df %>% arrange(pp) %>% 
-  mutate(rank = c(1:top_n, rep(NA, nrow(df)-top_n))) %>% View
 
 
 # Rank recalculation may result in inaccuracies since pp is interpolated
@@ -62,35 +52,57 @@ recalculate_rank <- function(df, top_n) {
 
 # To make plotting easier, "complete" dataframe by creating new rows: every combination of player and date 
 # Interpolate days and further interpolate frames
+# bar_race <- 
+#   player_history %>%
+#   filter(rank <= 10) %>%  # remove later
+#   complete(id, date) %>%
+#   group_by(id) %>%
+#   do(interpolate_consecutive(.)) %>%
+#   group_by(date) %>%
+#   do(recalculate_rank(., 10))
+
+
+
+# To make interpolation easier, "complete" dataframe by creating new rows: every combination of player and date 
+# interpolate: 
 bar_race <- 
   player_history %>%
-  filter(rank <= 10) %>%
-  complete(id, date) %>%
+  filter(rank <= 10 & date < ymd("20130101")) %>%  # for testing, remove later
+  complete(id, date = seq(min(date), max(date), by = "day")) %>%
+  arrange(date, desc(pp)) %>% # optional at this point
   group_by(id) %>%
-  do(interpolate_consecutive(.)) %>%
-  group_by(date) %>%
-  do(recalculate_rank(., 10))
+  mutate(country = tween_fill(country, "linear"),  # mutate_at?
+         username = tween_fill(username, "linear"),
+         accuracy = tween_fill(accuracy, "linear"),
+         playcount = tween_fill(playcount, "linear"),
+         pp = tween_fill(pp, "linear")
+         ) 
+
+#interpolate over frames
+
+
+# place bars manually 
+bar_race <-
+  bar_race %>% 
+  ungroup() %>%
+  group_by(date) %>% 
+  arrange(desc(pp)) %>%
+  mutate(rank = 1:n(),
+         bar_pos = rank)
 
 
 
-
-
-
-# animated bar chart (coord_flip is bugged)
-p <- ggplot(df, aes(y = rank, group = id)) + 
+# testing animated bar chart
+p <- ggplot(bar_race, aes(y = rank)) + 
   geom_tile(aes(x = pp / 2,
                 height = 0.9, width = pp), fill = "pink") +
-  geom_text(aes(x = 6000, label = username), 
+  geom_text(aes(x = 5000, label = username), 
             hjust = 0) + 
-  geom_text(aes(x = pp, label = as.character(pp)), # tweening creates decimals (issue #204)
+  geom_text(aes(x = pp, label = as.character(round(pp))),
             hjust = 1) +
-  ggtitle("{closest_state}") + 
-  scale_y_reverse() +
-  #coord_cartesian(expand = FALSE) + 
-  view_step_manual(xmin = 6000, xmax = 7000, ymin = 1, ymax = 10) +
+  coord_cartesian(xlim = c(5000, 7000), ylim = c(10, 1)) + 
   theme_minimal() +
-  transition_states(date, transition_length = 1, state_length = 1, wrap = FALSE) + 
-  ease_aes("cubic-in-out")
+  transition_manual(date)
 
-animate(p, fps = 10, duration = 40)
+animate(p, fps = 4, renderer = gifski_renderer())
  
