7243a8ee577c8b52e3be764a879cd80b5a907736
jxu <7989982+jxu@users.noreply.github.com>
2020-07-19 04:13:22 -0400

    Add bar race frames

--- visualize.R
+++ visualize.R
@@ -19,54 +19,38 @@ username_map <-
             current_username = username[which.max(date)])
 
 
-# Rank recalculation may result in inaccuracies since pp is interpolated
-recalculate_rank <- function(df, top_n) {
-  df %>% 
-    arrange(desc(pp)) %>% 
-    mutate(rank = c(1:top_n, rep(NA, nrow(df)-top_n)))
-}
-
-# To make plotting easier, "complete" dataframe by creating new rows: every combination of player and date 
-# Interpolate days and further interpolate frames
-# bar_race <- 
-#   player_history %>%
-#   filter(rank <= 10) %>%  # remove later
-#   complete(id, date) %>%
-#   group_by(id) %>%
-#   do(interpolate_consecutive(.)) %>%
-#   group_by(date) %>%
-#   do(recalculate_rank(., 10))
-
-
-
-# To make interpolation easier, "complete" dataframe by creating new rows: every combination of player and date 
+# To make interpolation easier, "complete" dataframe by creating new rows: 
+# every combination of player and date 
 # interpolate: 
-bar_race <- 
+history_day <- 
   player_history %>%
-  filter(rank <= 20 & date < ymd("20130101")) %>%  # for testing, remove later
+  filter(rank <= 20 & date < ymd("20120601")) %>%  # for testing, remove later
   complete(id, date = seq(min(date), max(date), by = "day")) %>%
   arrange(date, desc(pp)) %>% # optional at this point
   group_by(id) %>%
   mutate_at(vars(country, username, accuracy, playcount, pp), 
-            ~ tween_fill(., "linear"))
+            ~ tween_fill(., "linear")) %>%
+  ungroup()
 
 
-#interpolate over frames
-
+# create more frames between days
 bar_race <- 
-  bar_race %>%
-  mutate(frame_ = date)
+  history_day %>%
+  slice(rep(row_number(), 4)) %>%
+  group_by(id) %>%
+  arrange(date) %>%
+  mutate(frame = row_number()) %>%
+  ungroup()
 
 
-# place bars manually 
+# place bars manually, recalculating rank and bar positions for every frame
 bar_race <-
   bar_race %>% 
-  ungroup() %>%
-  group_by(date) %>% 
+  group_by(frame) %>% 
   arrange(desc(pp)) %>%
-  mutate(rank = 1:n(),
-         bar_pos = rank)
-
+  mutate(rank = row_number(),
+         bar_pos = rank) %>%
+  ungroup()
 
 
 # testing animated bar chart
@@ -77,11 +61,11 @@ p <- ggplot(bar_race, aes(y = rank)) +
             hjust = 0) + 
   geom_text(aes(x = pp, label = as.character(round(pp))),
             hjust = 1) +
-  geom_text(aes(x = 6000, y = 10, label = frame_), check_overlap = TRUE) +
+  geom_text(aes(x = 6000, y = 10, label = frame), check_overlap = TRUE) +
   scale_y_reverse(breaks = 1:10) + 
   coord_cartesian(xlim = c(5000, 7000), ylim = c(1, 10)) + 
   theme_minimal() +
-  transition_manual(frame_)
+  transition_manual(frame)
 
-animate(p, fps = 10, nframes = length(unique(bar_race$frame_)), renderer = gifski_renderer())
+animate(p, fps = 30, nframes = max(bar_race$frame), renderer = gifski_renderer())
  
